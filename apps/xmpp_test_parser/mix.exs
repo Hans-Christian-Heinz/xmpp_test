c58b6ef1598ff8430edcbd0ce179d1411b714af5
Hans-Christian-Heinz <christian@neubauer-heinz.de>
2021-01-13 14:57:04 +0000

    removed supervision-tree for XmppTestParser.

--- apps/xmpp_test_parser/mix.exs
+++ apps/xmpp_test_parser/mix.exs
@@ -21,7 +21,7 @@ defmodule XmppTestParser.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {XmppTestParser.Application, []}
+      # mod: {XmppTestParser.Application, []}
     ]
   end
 
